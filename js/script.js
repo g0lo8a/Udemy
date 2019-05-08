73c906fadf46eed1c6b9878c8fec4c702d8fac98
g0lo8a <c_76@bk.ru>
2019-05-08 09:39:19 +0300

    Задание 4

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let money, time;
 
 function start() {
     money = +prompt('Ваш бюджет на месяц?', 50000);
-    time = prompt('Введите дату в формате YYYY-MM-DD', '2019-05-04');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '2019-05-08');
 
     while (isNaN(money) || money == '' || money == null) {
         money = +prompt('Ваш бюджет на месяц?', 50000);
@@ -37,26 +37,49 @@ function chooseExpenses() {
 
 chooseExpenses();
 
-appData.moneyPerDay = +(appData.budget / 30).toFixed(2);
-
-z(`Бюджет на один день: ${appData.moneyPerDay}`);
-
-if (appData.moneyPerDay < 100) {
-    z('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    z('Средний уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    z('Высокий уровень достатка');
-} else {
-    z('Произошла ошибка');
+function chooseOptExpenses() {
+    let c;
+    for (let i = 0; i < 3; i++) {
+        c = prompt('Статья необязательных расходов?', 'bla-bla-bla');
+        if ((typeof (c)) === "string" && (typeof (c)) !== null && c.length < 50) {
+            appData.optionalExpenses[i + 1] = c;
+            z(i, c);
+        } else {
+            i = i - 1;
+        }
+    }
 }
+
+chooseOptExpenses();
+
+function detectDayBudget() {
+    appData.moneyPerDay = +(appData.budget / 30).toFixed(2);
+
+    z(`Бюджет на один день: ${appData.moneyPerDay}`);
+}
+
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        z('Минимальный уровень достатка');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        z('Средний уровень достатка');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        z('Высокий уровень достатка');
+    } else {
+        z('Произошла ошибка');
+    }
+}
+
+detectLevel();
 
 function checkSavings() {
 
     if (appData.savings === true) {
-        let save = +prompt('Какова сумма накоплений?'),
-            percent = +prompt('Под какой процент?');
-        appData.monthIncome = save / 100 / 12 * percent;
+        let save = +prompt('Какова сумма накоплений?', 35000),
+            percent = +prompt('Под какой процент?', 1);
+        appData.monthIncome = +(save / 100 / 12 * percent).toFixed(2);
 
         z(`Доход с депозита ${appData.monthIncome}`);
     }
